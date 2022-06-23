f9ccf1787b2505d3cc7c2611861b64f1c177e633
tenqube <kj.sa@tenqube.com>
2022-06-23 15:21:03 +0900

    android version 31 수정

--- app/src/main/java/com/tenqube/reward/MainFragment.kt
+++ app/src/main/java/com/tenqube/reward/MainFragment.kt
@@ -87,7 +87,7 @@ class MainFragment : Fragment() {
 
             override fun onPageFinished(view: WebView?, url: String?) {
                 super.onPageFinished(view, url)
-                webViewManager.setIsPageLoaded(true)
+//                webViewManager.setIsPageLoaded(true)
             }
         }
     }
